8b251ff5605426791548c676f79694818b1800f9
Taylor Nyquist <tnyquist@gmail.com>
2020-06-27 18:19:14 -0500

    initial html and css setup

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Pseudocode:
+
+// click start button: starts a timer, presents a question
+
+// when question is answered: presented another question
+
+// incorrect answer subtracts time from the clock
+
+// when all questions are answered OR timer reaches 0: game over
+
+// when game is over: save my initials and score
+
+// Layout:
+
+// top left header with link to view high scores
+
+// top right header with the timer
+
+// center container with questions and answer that is dynamic
