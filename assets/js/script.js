e1644e0f217508df27346e938d83de558e44536d
Taylor Nyquist <tnyquist@gmail.com>
2020-06-27 19:39:24 -0500

    added variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,72 @@
 // top right header with the timer
 
 // center container with questions and answer that is dynamic
+
+// Array of quiz questions:
+var quizQuestions = [
+    {
+        question: "In JavaScript, what element is used to store multiple values in a single variable?",
+        choices: ["Strings", "Arrays", "Variables", "Functions"],
+        answer: "Arrays"
+    },
+    {
+        question: "NaN stands for:",
+        choices: ["Numbers-aNonymous", "Not-any-Nannies", "Not-a-Number", "No-absolutely-Not"],
+        answer: "Not-a-Number"
+    },
+    {
+        question: "In JavaScript, what element is used to store and manipulate text?",
+        choices: ["Strings", "Variables", "Arrays", "Function"],
+        answer: "Strings"
+    },
+    {
+        question: "Where is the JavaScript placed inside an HTML document or page?",
+        choices: ["Body & Head", "Meta", "Title", "Footer"],
+        answer: "Body & Head"
+    },
+    {
+        question: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+        choices: ["Conditional Loop", "For Loop", "Else Loop", "While Loop"],
+        answer: "While Loop"
+    },
+    {
+        question: "What is considered to be the most popular programming lanugage in the world?",
+        choices: ["JavaScript", "Swift", "HTML", "Ruby"],
+        answer: "JavaScript"
+    },
+
+]
+
+// Defining global variables:
+var timerEl = document.querySelector("#timer");
+
+var questionEl = document.querySelector("#quiz-question");
+
+var choice1El = document.querySelector("#choice1").innerHTML;
+var choice2El = document.querySelector("#choice2").innerHTML;
+var choice3El = document.querySelector("#choice3").innerHTML;
+var choice4El = document.querySelector("#choice4").innerHTML;
+
+var resultEl = document.querySelector("result");
+
+var score = 0;
+
+// Loop over each question in the array
+for (var i=0; i < quizQuestions.length; i++) {
+    questionEl.textContent = quizQuestions[i].question;
+    // console.log(quizQuestions[i].question);
+};
+
+
+
+
+
+
+
+
+
+// Event listeners
+// choice1El.addEventListener("click", sendThisToAFunction);
+// choice2El.addEventListener("click", sendThisToAFunction);
+// choice3El.addEventListener("click", sendThisToAFunction);
+// choice4El.addEventListener("click", sendThisToAFunction);
